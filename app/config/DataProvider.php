e20adefdd5594ba998e554055f9041924dda03da
soufianenajim99 <soufianenajim100@gmail.com>
2023-12-18 09:53:40 +0100

    DataProvider

---
+++ app/config/DataProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+const CONFIG = [
+    'db'=>'mysql:host=localhost;dbname=e-banking-v2',
+    'db_user' => 'root',
+    'db_password' => ''
+];
+
+
+
+class DataProvider {
+    public function connect() {
+        try {
+            $dsn = CONFIG['db'];
+            $username = CONFIG['db_user'];
+            $password = CONFIG['db_password'];
+
+            return new PDO($dsn, $username, $password, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            ]);
+        } catch (PDOException $e) {
+            // Log or handle the exception appropriately
+            error_log('Connection failed: ' . $e->getMessage());
+            return null;
+        }
+    }
+}
+    ?>
